9cf24e7bdfb6934976c7dd1626c53a22d74494f2
Jacob Brady <jacob.brady0449@gmail.com>
2019-03-31 16:27:43 -0400

    images

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -50,7 +50,7 @@ exclude_patterns = []
 html_theme = 'alabaster'
 
 html_theme_options = {
-        'logo' : 'logo.png',
+        'logo' : './logo.png',
         'github_user': 'j-brady',
         'github_repo': 'peakipy'
         }
