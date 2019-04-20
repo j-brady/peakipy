9fa109bc8d5e902ee0f6200b3da9f025596e54e2
Jacob Brady <jacob.brady0449@gmail.com>
2019-04-20 16:41:13 -0400

    remove threshold adaptive

--- peakipy/core.py
+++ peakipy/core.py
@@ -1,4 +1,25 @@
-""" Functions for NMR peak deconvolution """
+""" 
+
+    peakipy - deconvolute overlapping NMR peaks
+    Copyright (C) 2019  Jacob Peter Brady
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+
+
 import sys
 from datetime import datetime
 from pathlib import Path
--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import os
 from glob import glob
-from distutils.core import setup
+from setuptools import setup
 
 if os.path.exists("build"):
     os.system("rm -r build")
 
-# scripts = glob("scripts/*.py")
 scripts = [
     "fit_peaks",
     "check_fits",
@@ -17,12 +16,40 @@ scripts = [
 scripts = [f"scripts/{script}" for script in scripts]
 [os.system(f"chmod +x {script}") for script in scripts]
 
+requirements = [
+        "pandas>=0.24.0",
+        "numpy>=1.16",
+        "matplotlib>=3.0",
+        "PyYAML>=3.13",
+        "nmrglue>=0.6.0",
+        "scipy>=1.2",
+        "docopt>=0.6.2",
+        "lmfit>=0.9.12",
+        "scikit-image>=0.14.2",
+        "bokeh>=1.0.4",
+        "schema>=0.7.0",
+        ]
+
 setup(
     name="peakipy",
-    version="0.1",
-    description="Some functions and scripts for deconvoluting nmrpeaks interactively",
+    version="0.1.2",
+    description="Some functions and scripts for deconvoluting NMR peaks interactively",
     author="Jacob Peter Brady",
     author_email="jacob.brady0449@gmail.com",
+    url="https://j-brady.github.io/peakipy",
     packages=["peakipy"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        ],
+    license="GNU GPLv3",
     scripts=scripts,
+    install_requires=requirements,
+    include_package_data=True,
 )
