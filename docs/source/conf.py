fd9022a4ddf6cefc910ac561a69d0a154de617ec
Jacob Brady <jacob.brady0449@gmail.com>
2019-06-27 22:37:27 -0400

    some more docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Jacob Peter Brady'
 author = 'Jacob Peter Brady'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.16'
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,6 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+        'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
---
+++ docs/source/usage/code.rst
@@ -0,0 +1,16 @@
+====
+Code
+====
+
+.. autofunction:: peakipy.core.gaussian
+
+.. autofunction:: peakipy.core.lorentzian
+
+.. autofunction:: peakipy.core.pseudo_voigt
+
+.. autofunction:: peakipy.core.make_mask
+
+.. autofunction:: peakipy.core.fit_first_plane
+
+.. autofunction:: peakipy.core.update_params
+
